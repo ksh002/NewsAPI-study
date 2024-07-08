6c35ac776f76b252d710346118b3aac5cbf69738
ksh002 <uio6625@naver.com>
2024-07-08 23:36:13 +0900

    url update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // const API_KEY = `8980b41bdcef4ff398c6d2d44ed8d165`
 let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const url = new URL(`https://newsapi-study.netlify.app/`);
     const response = await fetch(url)
     const data = await response.json()
     news = data.articles
